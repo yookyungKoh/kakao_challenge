caa0ea98ca58e0c0ec24fb4087a686662549639f
yookyung koh <yookyung@gPower2.gPower2>
2018-12-05 15:19:52 +0900

    DataLoader v1.0

--- utils.py
+++ utils.py
@@ -5,14 +5,28 @@ import os
 class KakaoDataset(Dataset):
     def __init__(self, data_root):
         data_path = os.path.join(data_root, 'data.h5py')
-        meta_path = os.path.join(data_root, 'meta')
-        self.data = h5py.File(data_path, 'r')
-        self.meta = cPickle.loads(open(meta_path, 'rb').read())
-        self.y = self.meta['y_vocab']
+        self.data = self._load_data(data_path)
+        self.X, self.y = self._read_data(self.data)
     
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, idx):
         return self.data[idx], self.y[idx]
+    
+    def _load_data(self, data_path):
+        return h5py.File(data_path)
 
+    def _read_data(self, data):
+        pid = data['pid']
+        text = data['uni']
+        freq = data['w_uni']
+        bcate = data['bcateid']
+        mcate = data['mcateid']
+        scate = data['scateid']
+        dcate = data['dcateid']
+        
+        X = (pid, text, freq)
+        y = (bcate, mcate, scate, dcate)
+
+        return X, y
