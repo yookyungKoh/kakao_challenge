794929f4bbc29c7b7fcf0d804624494322b5e4b0
hwisang <loststar0504@gmail.com>
2018-12-06 17:42:57 +0900

    fix dataset reinit

--- utils.py
+++ utils.py
@@ -6,6 +6,7 @@ import h5py
 class KakaoDataset(Dataset):
     def __init__(self, data_root, chunk_size=20000):
         data_path = os.path.join(data_root, 'data.h5py')
+        self.data_root = data_root
         self.data = self._load_data(data_path)
         self.chunk_size = chunk_size
         self.begin_offset = 0
@@ -18,10 +19,10 @@ class KakaoDataset(Dataset):
         self.m_chunk = self.data_ptr[3][self.begin_offset:self.end_offset]
         self.s_chunk = self.data_ptr[4][self.begin_offset:self.end_offset]
         self.d_chunk = self.data_ptr[5][self.begin_offset:self.end_offset]
-        self.total = len(self.t_chunk)
+        self.total = len(self.data_ptr[0])
 
     def __len__(self):
-        return len(self.t_chunk)
+        return self.total
 
     def __getitem__(self, idx):
         if not self.is_range(idx):
@@ -34,9 +35,12 @@ class KakaoDataset(Dataset):
             self.m_chunk = self.data_ptr[3][self.begin_offset:self.end_offset]
             self.s_chunk = self.data_ptr[4][self.begin_offset:self.end_offset]
             self.d_chunk = self.data_ptr[5][self.begin_offset:self.end_offset]
-        idx = self.begin_offset + idx
+        idx = idx - self.begin_offset
         X = list((self.t_chunk[idx], self.f_chunk[idx]))
         y = list((self.b_chunk[idx], self.m_chunk[idx], self.s_chunk[idx], self.d_chunk[idx]))
+
+        if idx + self.begin_offset + 1 >= self.total:
+            self.__init__(self.data_root, self.chunk_size)
 
         return X, y
 
