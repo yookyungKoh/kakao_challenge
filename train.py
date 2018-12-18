8a39c85049c445a2dd00d9d533e19a0fd123fc1f
yookyung koh <yookyung@gPower2.gPower2>
2018-12-18 20:59:29 +0900

    validation available

--- train.py
+++ train.py
@@ -9,7 +9,7 @@ import h5py
 import json
 import six
 
-from mlp_model import MLP
+from mlp_model import MLP, HMCN, make_layer
 from utils import KakaoDataset
 from misc import Option, get_logger
 
@@ -42,9 +42,9 @@ dev_loader = DataLoader(dev_dataset, batch_size=opt.batch_size)
 print('# training samples: {:,}'.format(len(train_dataset)))
 print('# validation samples: {:,}'.format(len(valid_dataset)))
 
-save_model_path = './model/checkpoints/mlp_1'
-best_model_path = './model/best/mlp_1_best.pth'
-result_path = './results/mlp_1_result.tsv'
+save_model_path = './model/checkpoints/mlp_2'
+best_model_path = './model/best/mlp_2_best.pth'
+result_path = './results/mlp_2_result.tsv'
 continue_train = True
 
 def get_acc(x, y):
@@ -55,7 +55,7 @@ def get_acc(x, y):
     return acc
 
 def get_score(b_acc, m_acc, s_acc, d_acc, i):
-    bw, mw, sw, dw = 1, 1.2, 1.3, 1.4
+    bw, mw, sw, dw = 1., 1.2, 1.3, 1.4
     score = (b_acc*bw + m_acc*mw + s_acc*sw + d_acc*dw)/4
     return score/(i+1)
 
@@ -65,32 +65,32 @@ def main():
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     # model 
-    model = MLP(opt).to(device)
+    model = HMCN(opt).to(device)
     criterion = nn.BCEWithLogitsLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=opt.lr, betas=(0.9, 0.999))
     num_params = sum([p.numel() for p in model.parameters()])
     print('Total # of params: {:,}'.format(num_params))
 
-#    if continue_train == True:
-#        model.load_state_dict(torch.load(save_model_path+'_E75.pth'))
-#    
-#    opt.num_epochs = 25
-#    best_loss = 100000.
-#    for epoch in range(opt.num_epochs):
-#        train(opt, train_loader, model, criterion, optimizer, epoch) 
+    if continue_train == True:
+        model.load_state_dict(torch.load(best_model_path))
+  
+    model.load_state_dict(torch.load(best_model_path)) 
+    opt.num_epochs = 55 
+    best_loss = 100000.
+    for epoch in range(opt.num_epochs):
+        train(opt, train_loader, model, criterion, optimizer, epoch) 
+        val_loss = evaluate(opt, valid_loader, model, criterion, make_file=False)
 
-#        if val_loss < best_loss:
-#            best_loss = val_loss
-#            torch.save(model.state_dict(), best_model_path)
-#            print('model saved at loss: %.4f'%(best_loss))
-        
-#        if (epoch+1) % 5 == 0:
-#            torch.save(model.state_dict(), save_model_path + '_E%d.pth'%(epoch++75+1))
-#
-#    val_loss = evaluate(opt, valid_loader, model, criterion)
-
-    model.load_state_dict(torch.load(save_model_path+'_E100.pth'))
-    dev_loss = evaluate(opt, dev_loader, model, criterion)
+        if val_loss < best_loss:
+            best_loss = val_loss
+            torch.save(model.state_dict(), best_model_path)
+            print('model saved at loss: %.4f'%(best_loss))
+       
+        if (epoch+1) % 5 == 0:
+            torch.save(model.state_dict(), save_model_path + '_E%d.pth'%(epoch+45+1))
+    
+    model.load_state_dict(torch.load(best_model_path))
+    dev_loss = evaluate(opt, dev_loader, model, criterion, make_file=True)
 
     pid_order = []
     h = h5py.File('./data/dev/data.h5py', 'r')['dev']
@@ -106,7 +106,7 @@ def main():
             f.write('\n')
     print('created file at %s'%(result_path))
 
-def train(opt, dataloader, model, criterion, optimizer, epoch):
+def train(opt, dataloader, model, criterion, optimizer, epoch, beta=0.5):
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     start_time = time.time()
 
@@ -117,32 +117,40 @@ def train(opt, dataloader, model, criterion, optimizer, epoch):
 
     for i, (inputs, targets) in enumerate(dataloader):
         targets = [t.type(torch.FloatTensor).to(device) for t in targets]
+        g_targets = torch.cat((targets[0], targets[1], targets[2], targets[3]), dim=1)
+
         out = model(inputs)
+        g_out = out[4]
+        l_out = torch.cat((out[0], out[1], out[2], out[3]), dim=1)
+        f_out = beta * g_out + (1-beta) * l_out
 
         bloss = criterion(out[0], targets[0])
         mloss = criterion(out[1], targets[1])
         sloss = criterion(out[2], targets[2])
-        dloss = criterion(out[3], targets[3])
-        total_loss = bloss + mloss + sloss + dloss
+        dloss = criterion(out[3], targets[3])    
+        l_loss = bloss + mloss + sloss + dloss
+        g_loss = criterion(g_out, g_targets)
+
+        total_loss = l_loss + g_loss
         train_loss += total_loss.item()
         
-        acc1 += get_acc(out[0], targets[0]).item()
-        acc2 += get_acc(out[1], targets[1]).item()
-        acc3 += get_acc(out[2], targets[2]).item()
-        acc4 += get_acc(out[3], targets[3]).item()
+        acc1 += get_acc(f_out[:,0:57], targets[0]).item()
+        acc2 += get_acc(f_out[:,57:57+552], targets[1]).item()
+        acc3 += get_acc(f_out[:,57+552:57+552+3190], targets[2]).item()
+        acc4 += get_acc(f_out[:,57+552+3190:], targets[3]).item()
         train_score += get_score(acc1, acc2, acc3, acc4, i)
 
         model.zero_grad()
         total_loss.backward()
         optimizer.step()
 
-        out_str.write('Epoch [%d/%d] | Step [%d/%d] | Loss: %.4f | Score: %.4f | b: %.4f | m: %.4f | s: %.4f | d: %.4f \r' 
-                    %(epoch+1, opt.num_epochs, i+1, len(dataloader), train_loss/(i+1), train_score/(i+1), acc1/(i+1), acc2/(i+1), acc3/(i+1), acc4/(i+1)))
+        out_str.write('Epoch [%d/%d] | Step [%d/%d] | Loss: %.4f e-4 | Score: %.4f | b: %.4f | m: %.4f | s: %.4f | d: %.4f \r' 
+                    %(epoch+1, opt.num_epochs, i+1, len(dataloader), train_loss*1e4/(i+1), train_score/(i+1), acc1/(i+1), acc2/(i+1), acc3/(i+1), acc4/(i+1)))
         out_str.flush()
 
     print('Training time: %.2f'%((time.time()-start_time)/ 60))
 
-def evaluate(opt, dataloader, model, criterion):
+def evaluate(opt, dataloader, model, criterion, make_file=False, beta=0.5):
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     valid_loss = 0.
     acc1, acc2, acc3, acc4 = 0., 0., 0., 0.
@@ -153,35 +161,43 @@ def evaluate(opt, dataloader, model, criterion):
             start_idx = idx
             end_idx = start_idx + len(inputs[0])
             targets = [t.type(torch.FloatTensor).to(device) for t in targets]
+            g_targets = torch.cat((targets[0], targets[1], targets[2], targets[3]), dim=1)
+
             out = model(inputs)
-            
+            g_out = out[4]
+            l_out = torch.cat((out[0], out[1], out[2], out[3]), dim=1)
+            f_out = beta * g_out + (1-beta) * l_out
+
             bloss = criterion(out[0], targets[0])
             mloss = criterion(out[1], targets[1])
             sloss = criterion(out[2], targets[2])
-            dloss = criterion(out[3], targets[3])
-            total_loss = bloss + mloss + sloss + dloss
-            valid_loss += total_loss.item()
+            dloss = criterion(out[3], targets[3])    
+            l_loss = bloss + mloss + sloss + dloss
+            g_loss = criterion(g_out, g_targets)
 
-            acc1 += get_acc(out[0], targets[0]).item()
-            acc2 += get_acc(out[1], targets[1]).item()
-            acc3 += get_acc(out[2], targets[2]).item()
-            acc4 += get_acc(out[3], targets[3]).item()
+            total_loss = l_loss + g_loss
+            valid_loss += total_loss.item()
+        
+            acc1 += get_acc(f_out[:,0:57], targets[0]).item()
+            acc2 += get_acc(f_out[:,57:57+552], targets[1]).item()
+            acc3 += get_acc(f_out[:,57+552:57+552+3190], targets[2]).item()
+            acc4 += get_acc(f_out[:,57+552+3190:], targets[3]).item()
             valid_score += get_score(acc1, acc2, acc3, acc4, i)
 
-            out_str.write('Step [%d/%d] | Loss: %.4f | Score: %.4f | cate1: %.4f | cate2: %.4f | cate3: %.4f | cate4: %.4f \r' 
-                        %(i+1, len(dataloader), valid_loss/(i+1), valid_score/(i+1), acc1/(i+1), acc2/(i+1), acc3/(i+1), acc4/(i+1)))
-            out_str.flush()
-            
-            idx = end_idx
-            out_str.write('start: %d | end: %d \r' %(start_idx, end_idx))
+            out_str.write('Step [%d/%d] | Loss: %.4f e-4 | Score: %.4f | b: %.4f | m: %.4f | s: %.4f | d: %.4f \r' 
+                        %(i+1, len(dataloader), valid_loss*1e4/(i+1), valid_score/(i+1), acc1/(i+1), acc2/(i+1), acc3/(i+1), acc4/(i+1)))
             out_str.flush()
 
-#            create_pred_file(train_path, start_idx, end_idx, out, result_path, inv_cate1)
+            if make_file:
+                idx = end_idx
+                out_str.write('start: %d | end: %d \r' %(start_idx, end_idx))
+                out_str.flush()
+                create_pred_file(dev_path, start_idx, end_idx, f_out, result_path, inv_cate1)
 
     valid_loss /= (i+1)
     return valid_loss
 
-def create_pred_file(data_path, start_idx, end_idx, out, out_path, inv_cate1):
+def create_pred_file(data_path, start_idx, end_idx, f_out, out_path, inv_cate1):
     """
     idx : to crop pid
     out : model output (tuple)
@@ -195,10 +211,10 @@ def create_pred_file(data_path, start_idx, end_idx, out, out_path, inv_cate1):
     pid_order.extend(h['pid'][::])
     pid_batch = pid_order[start_idx : end_idx] #(N)
 
-    b = torch.max(out[0],1)[1] #(N)
-    m = torch.max(out[1],1)[1]
-    s = torch.max(out[2],1)[1]
-    d = torch.max(out[3],1)[1]
+    b = torch.max(f_out[:,0:57],1)[1] #(N)
+    m = torch.max(f_out[:,57:57+552],1)[1]
+    s = torch.max(f_out[:,57+552:57+552+3190],1)[1]
+    d = torch.max(f_out[:,57+552+3190:],1)[1]
     b, m, s, d = list(map(lambda x: x+1, (b,m,s,d)))
     s[s==1] = -1
     d[d==1] = -1
