5ea40027ee8523965e4e0da316bc72a6be5cd1dc
yookyung koh <yookyung@gPower2.gPower2>
2018-12-18 20:58:14 +0900

    HMCN model updated

--- mlp_model.py
+++ mlp_model.py
@@ -64,9 +64,7 @@ class MLP(nn.Module):
         y1 = self.b_emb(y1) #(N, 10)
     
         m_all = self.m_emb.weight
-#        m_all = m_all.expand(y1.size(0), self.cate2_size, self.cate_emb_size) #(N, 552, 10)
         sim_bm = torch.matmul(m_all, y1.unsqueeze(2))
-#        sim_bm = torch.bmm(m_all, y1.unsqueeze(2)) #(N, 552, 1)
         sim_bm = sim_bm.squeeze() #(N, 552)
 
         h2 = torch.cat((h1, y1), dim=1) #(N, 138)
@@ -76,9 +74,7 @@ class MLP(nn.Module):
         y2 = self.m_emb(y2)
 
         s_all = self.s_emb.weight
-#        s_all = s_all.expand(y2.size(0), self.cate3_size, self.cate_emb_size) #(N, 3190, 10)
         sim_ms = torch.matmul(s_all, y2.unsqueeze(2))
-#        sim_ms = torch.bmm(s_all, y2.unsqueeze(2))
         sim_ms = sim_ms.squeeze()
 
         h3 = torch.cat((h2, y2), dim=1) #(N, 148)
@@ -88,9 +84,7 @@ class MLP(nn.Module):
         y3 = self.s_emb(y3)
 
         d_all = self.d_emb.weight
-#        d_all = d_all.expand(y3.size(0), self.cate4_size, self.cate_emb_size) #(N, 404, 10)
         sim_sd = torch.matmul(d_all, y3.unsqueeze(2))
-#        sim_sd = torch.bmm(d_all, y3.unsqueeze(2))
         sim_sd = sim_sd.squeeze()
 
         h4 = torch.cat((h3, y3), dim=1) #(N, 158)
@@ -99,4 +93,70 @@ class MLP(nn.Module):
         
         return out1, out2, out3, out4
 
+def make_layer(in_dim, out_dim):
+    return nn.Sequential(
+            nn.Linear(in_dim, out_dim),
+            nn.ReLU(),
+            nn.BatchNorm1d(out_dim))
+
+class HMCN(nn.Module):
+    def __init__(self, opt):
+        super(HMCN, self).__init__()
+        vocab_size = opt.unigram_hash_size + 1
+        max_len = opt.max_len
+        num_classes = [57, 552, 3190, 404]
+        self.embd = nn.Embedding(vocab_size, opt.embd_size)
         
+        self.g1 = make_layer(opt.embd_size, opt.embd_size)
+        self.g2 = make_layer(opt.embd_size, opt.embd_size)
+        self.g3 = make_layer(opt.embd_size, opt.embd_size)
+        self.g4 = make_layer(opt.embd_size, opt.embd_size)
+
+        self.g = nn.Linear(opt.embd_size, sum(num_classes))
+
+        self.l1 = make_layer(opt.embd_size, opt.embd_size)
+        self.l2 = make_layer(opt.embd_size, opt.embd_size)
+        self.l3 = make_layer(opt.embd_size, opt.embd_size)
+        self.l4 = make_layer(opt.embd_size, opt.embd_size)
+
+        self.p1 = nn.Linear(opt.embd_size, num_classes[0])
+        self.p2 = nn.Linear(opt.embd_size, num_classes[1])
+        self.p3 = nn.Linear(opt.embd_size, num_classes[2])
+        self.p4 = nn.Linear(opt.embd_size, num_classes[3])
+
+        self.dropout = nn.Dropout(0.6)
+        
+    def forward(self, inputs):
+        word_idx = inputs[0].type(torch.LongTensor).to(device) #(N, max_len)
+        freq = inputs[1].type(torch.FloatTensor).to(device) #(N, max_len)
+
+        word_embed = self.embd(word_idx) #(N, max_len, emb_size)
+        text_feature = torch.bmm(word_embed.permute(0,2,1), freq.unsqueeze(2)) #(N, 128, 1)
+        h1 = text_feature.squeeze() #(N, 128)
+
+        # global
+        # (FC > relu > BN -> residual -> dropout)
+        a_g_1 = self.dropout(self.g1(h1))
+        a_l_1 = self.l1(a_g_1)
+        out1 = self.p1(a_l_1) # b out
+        a_g_1 = a_g_1 + h1
+
+        a_g_2 = self.dropout(self.g2(a_g_1))
+        a_l_2 = self.l2(a_g_2)
+        out2 = self.p2(a_l_2) # m out
+        a_g_2 = a_g_2 + h1
+
+        a_g_3 = self.dropout(self.g3(a_g_2))
+        a_l_3 = self.l3(a_g_3)
+        out3 = self.p3(a_l_3) # s out
+        a_g_3 = a_g_3 + h1
+
+        a_g_4 = self.dropout(self.g4(a_g_3))
+        a_l_4 = self.l4(a_g_4)
+        out4 = self.p4(a_l_4) # d out 
+        a_g_4 = a_g_4 + h1
+        
+        g_out = self.g(a_g_4) # global out #(N, 4203) 
+
+        return out1, out2, out3, out4, g_out
+
